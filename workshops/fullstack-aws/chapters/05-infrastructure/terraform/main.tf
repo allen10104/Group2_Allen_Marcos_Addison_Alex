026735717c4b6b61a43154eea62dfb34a5df033e
Chandan Kumar <chandank@becloudready.com>
2026-08-18 12:53:34 -0400

    Removed stale submissions

---
+++ workshops/fullstack-aws/chapters/05-infrastructure/terraform/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+variable "created_date" {
+  description = "Creation date for the `date` tag, format dd-mmm-yyyy (e.g. 12-Jul-2026)."
+  type        = string
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      workshop   = "full-stack"
+      autodelete = "true"
+      date       = var.created_date
+    }
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-002192a70217ac181" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "<>>-tf-instance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+}
